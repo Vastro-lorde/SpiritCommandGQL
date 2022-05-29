a357c9071b446deed649badc97e62d283ba554fb
Vastro-lorde <omatsolaseund@outlook.com>
2022-05-29 08:48:16 +0100

    Add project files.

---
+++ SpiritCommandGQL/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpiritCommandGQL.Models;
+
+namespace SpiritCommandGQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<Kernel> Kernels { get; set; }
+    }
+}
---
+++ SpiritCommandGQL/Models/Kernel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpiritCommandGQL.Models
+{
+    public class Kernel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string LicenceKey { get; set; }
+    }
+}
